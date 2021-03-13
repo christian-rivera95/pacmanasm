dcdbb6df910deafefa06957116f4fe5f5df36b3a
christian-rivera95 <chris.rivera9582@gmail.com>
2021-03-12 19:33:14 -0600

    fibonacci

--- fibonacci_hex.asm
+++ fibonacci_hex.asm
@@ -6,7 +6,7 @@ fibonacci:
     li $r4, 1 //b
     li $r5, 0 //c
     li $r6, 2 //i
-    $r7, $r3, $r0
+    add $r7, $r3, $r0
     beq $r2, $r0,looper  //if(n==0)end
     for_start:
         beq $r6, $r2,for_end  //if(i==n)end
