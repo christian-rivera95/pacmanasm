fb8a7b0e771722878633c0332808ce66d750099f
christian-rivera95 <chris.rivera9582@gmail.com>
2021-03-12 19:52:08 -0600

    fibonacci

--- fibonacci_hex.asm
+++ fibonacci_hex.asm
@@ -9,11 +9,11 @@ fibonacci:
     add $r7, $r3, $r0
     beq $r2, $r0,looper  //if(n==0)end
     for_start:
-        slt $r0, $r2, $r5
-        beq $r6, $r1,for_end  //if(i==n)end
+        slt $r0, $r2, $r6
+        beq $r0, $r1,for_end  //if(i==n)end
 
         li $r0, 0
-        
+
         add $r5, $r3, $r4
         add $r3, $r4, $r0
         add $r4, $r5, $r0
