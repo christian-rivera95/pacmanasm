74e77233e452d4c663bb901efa1ccf3ff35c7f75
christian-rivera95 <chris.rivera9582@gmail.com>
2021-03-12 19:28:45 -0600

    fibonacci

--- fibonacci_hex.asm
+++ fibonacci_hex.asm
@@ -1,15 +1,15 @@
 fibonacci:
     li $r0, 0
     li $r1, 1
-    li $r2, 5 //n
+    li $r2, 9 //n
     li $r3, 0 //a
     li $r4, 1 //b
     li $r5, 0 //c
     li $r6, 2 //i
+    $r7, $r3, $r0
+    beq $r2, $r0,looper  //if(n==0)end
     for_start:
-        $r7, $r3, $r0
-        beq $r2, $r0,looper
-        beq $r6, $r2,for_end
+        beq $r6, $r2,for_end  //if(i==n)end
         add $r5, $r3, $r4
         add $r3, $r4, $r0
         add $r4, $r5, $r0
